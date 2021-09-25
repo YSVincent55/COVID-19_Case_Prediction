fdb735dd28ac8595e0d6ed3a04fc8aa78d44967b
Aw Yew Lim <yewlim00@gmail.com>
2021-09-25 20:57:08 +0800

    Update format of Q4_kedah

--- Question4/Q4_kedah.py
+++ Question4/Q4_kedah.py
@@ -10,8 +10,9 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.metrics import mean_squared_error as mse, mean_absolute_error, roc_curve, accuracy_score, classification_report, roc_auc_score, precision_recall_curve
+import plotly.graph_objects as go
 
-def model_kedah():
+def q4_kedah():
     final_merged = pd.read_csv('Dataset/final_merged.csv')
     strong_feat = ['pcr', 'positivity_rate', 'cases_recovered', 'hosp_pui',
        'pkrc_admitted_total', 'vent_noncovid', 'rtk-ag',
@@ -34,128 +35,150 @@ def model_kedah():
 
     left_column1, right_column1 = st.columns(2)
     with left_column1:
-        left_column1.header('Catboost')
         cbr = pickle.load(open('Model/cbr_kedah', 'rb'))
         y_kedah_predcbr =cbr.predict(X_kedah_test)
+        cbr_r2 = cbr.score(X_kedah_test,y_kedah_test)
+        cbr_mae = mean_absolute_error(y_kedah_test, y_kedah_predcbr)
         cbr_mse = mse(y_kedah_test, y_kedah_predcbr)
         cbr_rmse = mse(y_kedah_test, y_kedah_predcbr, squared=False)
-        st.markdown("r2: " + str(cbr.score(X_kedah_test,y_kedah_test)))
-        st.markdown('mae: ' + str(mean_absolute_error(y_kedah_test, y_kedah_predcbr)))
-        st.markdown('mse: ' + str(cbr_mse))
-        st.markdown('rmse: ' + str(cbr_rmse))
-        #st.markdown('Actual: ' + str(y_kedah_test.values))
-        #st.markdown('Predicted: ' + str(y_kedah_predcbr))
+
+        st.markdown('** CatBoost Regressor Score **')
+        table1 = go.Figure(data=[go.Table(
+        columnwidth=[1, 4],
+        header=dict(values=['Metrics', 'Value'],
+                    fill=dict(color=['paleturquoise']),
+                    align='center',height=30),
+        cells=dict(values=[['R2','MAE','MSE','RMSE'],
+                        [str(cbr_r2),str(cbr_mae), str(cbr_mse), str(cbr_rmse)]],
+                fill=dict(color=['lightcyan']),
+                align='center',height=30))
+        ])
+        table1 = table1.update_layout(width=400,height=200, margin=dict(l=0,r=10,t=5,b=0))
+        st.write(table1)
 
     with right_column1:
-        right_column1.header('Linear Regression')
         lr = pickle.load(open('Model/lr_kedah', 'rb'))
         y_kedah_predlr =lr.predict(X_kedah_test)
+        lr_r2 = lr.score(X_kedah_test,y_kedah_test)
+        lr_mae = mean_absolute_error(y_kedah_test, y_kedah_predlr)
         lr_mse = mse(y_kedah_test, y_kedah_predlr)
         lr_rmse = mse(y_kedah_test, y_kedah_predlr, squared=False)
-        st.markdown("r2: " + str(lr.score(X_kedah_test,y_kedah_test)))
-        st.markdown('mae: ' + str(mean_absolute_error(y_kedah_test, y_kedah_predlr)))
-        st.markdown('mse: ' + str(lr_mse))
-        st.markdown('rmse: ' + str(lr_rmse))
-        #st.markdown('Actual: ' + str(y_kedah_test.values))
-        #st.markdown('Predicted: ' + str(y_kedah_predlr))
 
+        st.markdown('** Linear Regressor Score **')
+        table2 = go.Figure(data=[go.Table(
+        columnwidth=[1, 4],
+        header=dict(values=['Metrics', 'Value'],
+                    fill=dict(color=['paleturquoise']),
+                    align='center',height=30),
+        cells=dict(values=[['R2','MAE','MSE','RMSE'],
+                        [str(lr_r2),str(lr_mae), str(lr_mse), str(lr_rmse)]],
+                fill=dict(color=['lightcyan']),
+                align='center',height=30))
+        ])
+        table2 = table2.update_layout(width=400,height=200, margin=dict(l=0,r=10,t=5,b=0))
+        st.write(table2)
 
     y_kedah_bin =pd.cut(y_kedah,3,labels=['Low','Medium','High'])
     X_kedah_train, X_kedah_test, y_kedah_train, y_kedah_test = train_test_split(X_kedah_norm, y_kedah_bin, test_size=0.3, random_state=2)
 
+    with open('Model/lgbm_kedah', 'rb') as file:  
+        lgbm = pickle.load(file)
 
-    st.header('Classification Model')
-    left_column2, right_column2 = st.columns(2)
+    y_pred_lgbm  = lgbm.predict(X_kedah_test)
+    class_lgbm = classification_report(y_kedah_test, y_pred_lgbm)
 
-    # Confusion Matrix
+    st.markdown('## Classification')
+    st.markdown('** LGBM Classifier **')
 
-    with left_column2:
-        left_column2.header('LGBM')
-        
-        lgbm = pickle.load(open('Model/lgbm_kedah', 'rb'))
-        y_pred_lgbm  = lgbm.predict(X_kedah_test)
-        st.write(classification_report(y_kedah_test, y_pred_lgbm))
+    st.write(class_lgbm)
 
-        fig1,ax1 = plt.subplots(figsize = (20,10))
-        cf_matrix = confusion_matrix(y_kedah_test, y_pred_lgbm)
-        sns.heatmap(cf_matrix, annot=True)
+    fig1,ax1 = plt.subplots(figsize = (10,10))
+    ax1.set_title("Confusion Matrix for LGBM Classifier")
+    conf_matrix_lgbm = confusion_matrix(y_kedah_test, y_pred_lgbm)
+    lgbm_display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix_lgbm,display_labels = lgbm.classes_)
+    lgbm_display.plot(cmap = 'Greens',xticks_rotation ='vertical',ax=ax1)
+    st.pyplot(fig1)
 
-        st.pyplot(fig1)
-        
+    with open('Model/rfc_kedah', 'rb') as file:  
+        rfc = pickle.load(file)
 
-    with right_column2:
-        right_column2.header('Random Forest Classifier')
-        rfc = pickle.load(open('Model/rfc_kedah', 'rb'))
-        y_pred_rfc = rfc.predict(X_kedah_test)
-        st.write(classification_report(y_kedah_test, y_pred_rfc))
+    y_pred_rfc = rfc.predict(X_kedah_test)
+    class_rf = classification_report(y_kedah_test, y_pred_rfc)
 
-        fig2,ax2 = plt.subplots(figsize = (20,10))
-        cf_matrix = confusion_matrix(y_kedah_test, y_pred_rfc)
-        sns.heatmap(cf_matrix, annot=True)
-        st.pyplot(fig2)
+    st.markdown('** Random Forest Classifier **')
+    st.write(class_rf)
 
-    # ROC
+    fig2,ax2 = plt.subplots(figsize = (10,10))
+    ax2.set_title("Confusion Matrix for Random Forest Classifier")
+    conf_matrix_rfc = confusion_matrix(y_kedah_test, y_pred_rfc)
+    rfc_display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix_rfc,display_labels = rfc.classes_)
+    rfc_display.plot(cmap = 'Greens',xticks_rotation ='vertical',ax=ax2)
+    st.pyplot(fig2)
 
-    with left_column2:
-        fig1,ax1 = plt.subplots(figsize = (20,10))
+    encoder = LabelEncoder()
+    y_kedah_test = encoder.fit_transform(y_kedah_test)
 
-        encoder = LabelEncoder()
-        y_kedah_test = encoder.fit_transform(y_kedah_test)
-        pred_prob1 = lgbm.predict_proba(X_kedah_test)[:,1]
-        fpr1, tpr1, thresh1 = roc_curve(y_kedah_test, pred_prob1, pos_label=1)
-        random_probs = [0 for i in range(len(y_kedah_test))]
-        p_fpr, p_tpr, _ = roc_curve(y_kedah_test, random_probs, pos_label=1)
-        ax1.plot(fpr1, tpr1, linestyle='--',color='orange', label='LGBM')
-        ax1.plot(p_fpr, p_tpr, linestyle='--', color='black')
-        ax1.set_title('ROC curve')
-        ax1.set_xlabel('False Positive Rate')
-        ax1.set_ylabel('True Positive rate')
-        ax1.legend(loc='best')
+    pred_prob1 = lgbm.predict_proba(X_kedah_test)[:,1]
+    pred_prob2 = rfc.predict_proba(X_kedah_test)[:,1]
 
-        st.pyplot(fig1)
+    fpr1, tpr1, thresh1 = roc_curve(y_kedah_test, pred_prob1, pos_label=1)
+    fpr2, tpr2, thresh2 = roc_curve(y_kedah_test, pred_prob2, pos_label=1)
+    prec_lgbm, rec_lgbm, threshold_lgbm = precision_recall_curve(y_kedah_test, pred_prob1,pos_label=1)
+    prec_rfc, rec_rfc, threshold_rfc = precision_recall_curve(y_kedah_test, pred_prob2,pos_label=1)
+    random_probs = [0 for i in range(len(y_kedah_test))]
+    p_fpr, p_tpr, _ = roc_curve(y_kedah_test, random_probs, pos_label=1)
 
-    with right_column2:
-        fig2,ax2 = plt.subplots(figsize = (20,10))
+    roc_lgbm = pd.DataFrame()
+    roc_lgbm['fpr'] = fpr1
+    roc_lgbm['tpr'] = tpr1
+    roc_lgbm['Type'] = 'LGBM'
 
-        encoder = LabelEncoder()
-        y_kedah_test = encoder.fit_transform(y_kedah_test)
-        pred_prob2 = rfc.predict_proba(X_kedah_test)[:,1]
-        fpr2, tpr2, thresh2 = roc_curve(y_kedah_test, pred_prob2, pos_label=1)
-        random_probs = [0 for i in range(len(y_kedah_test))]
-        p_fpr, p_tpr, _ = roc_curve(y_kedah_test, random_probs, pos_label=1)
-        ax2.plot(fpr2, tpr2, linestyle='--',color='green', label='Random Forest Classifier')
-        ax2.plot(p_fpr, p_tpr, linestyle='--', color='black')
-        ax2.set_title('ROC curve')
-        ax2.set_xlabel('False Positive Rate')
-        ax2.set_ylabel('True Positive rate')
-        ax2.legend(loc='best')
+    roc_rf = pd.DataFrame()
+    roc_rf['fpr'] = fpr2
+    roc_rf['tpr'] = tpr2
+    roc_rf['Type'] = 'Random Forest'
 
-        st.pyplot(fig2)
+    roc_all = pd.concat([roc_lgbm,roc_rf])
 
-    # Precision Recall Curve
+    roc_p = pd.DataFrame()
+    roc_p['fpr'] = p_fpr
+    roc_p['tpr'] = p_tpr
 
-    with left_column2:
-        fig1,ax1 = plt.subplots(figsize = (20,10))
+    prc_lgbm = pd.DataFrame()
+    prc_lgbm['prec'] = prec_lgbm
+    prc_lgbm['rec'] = rec_lgbm
+    prc_lgbm['Type'] = 'LGBM'
 
-        prec_lgbm, rec_lgbm, threshold_lgbm = precision_recall_curve(y_kedah_test, pred_prob1,pos_label=1)
-        ax1.plot(rec_lgbm, prec_lgbm, color='orange', label='LGBM Classifier') 
-        ax1.plot([1, 0], [0.1, 0.1], color='black', linestyle='--')
-        ax1.set_xlabel('Recall')
-        ax1.set_ylabel('Precision')
-        ax1.set_title('Precision-Recall Curve')
-        ax1.legend()
+    prc_rf = pd.DataFrame()
+    prc_rf['prec'] = prec_rfc
+    prc_rf['rec'] = rec_rfc
+    prc_rf['Type'] = 'Random Forest'
 
-        st.pyplot(fig1)
+    prc_all = pd.concat([prc_lgbm,prc_rf])
 
-    with right_column2:
-        fig2,ax2 = plt.subplots(figsize = (20,10))
+    st.markdown('## Perfomance Comparison')
 
-        prec_rfc, rec_rfc, threshold_rfc = precision_recall_curve(y_kedah_test, pred_prob2,pos_label=1)
-        ax2.plot(rec_rfc, prec_rfc, color='green', label='Random Forest Classifier') 
-        ax2.plot([1, 0], [0.1, 0.1], color='black', linestyle='--')
-        ax2.set_xlabel('Recall')
-        ax2.set_ylabel('Precision')
-        ax2.set_title('Precision-Recall Curve')
-        ax2.legend()
+    chart_all = alt.Chart(roc_all).mark_line().encode(
+                                                    alt.X('fpr', title="False Positive Rate"),
+                                                    alt.Y('tpr', title="True Positive Rate"),
+                                                    alt.Color('Type', type='nominal'))
 
-        st.pyplot(fig2)
+    chart_p = alt.Chart(roc_p).mark_line(strokeDash=[20,5], color = 'black').encode(
+                                                                    alt.X('fpr'),
+                                                                    alt.Y('tpr'))
+
+    roc_chart =chart_all + chart_p.properties(title='ROC curve',width=800,height=600,)
+    st.altair_chart(roc_chart)
+
+    fig4, ax4 = plt.subplots()
+    prec_lgbm, rec_lgbm, threshold_lgbm = precision_recall_curve(y_kedah_test, pred_prob1,pos_label=1)
+    prec_rfc, rec_rfc, threshold_rfc = precision_recall_curve(y_kedah_test, pred_prob2,pos_label=1)
+    plt.plot(rec_lgbm,prec_lgbm, color='orange', label='LGBM Classifier') 
+    plt.plot(rec_rfc, prec_rfc, color='green', label='Random Forest Classifier') 
+    baseline1 = len(y_kedah_test[y_kedah_test==1]) / len(y_kedah_test)
+    plt.plot([1, 0], [baseline1,baseline1], color='black', linestyle='--')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve')
+    plt.legend()
+    st.pyplot(fig4)
